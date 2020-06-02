55a38f43089aa52d090984287bed5d5e25d60001
Michael Barbour <barbour.michael.0@gmail.com>
2020-06-01 22:16:36 -0400

    virtual_bass.c: Some algorithm tweaks (filtering, mostly).

--- virtual_bass.c
+++ virtual_bass.c
@@ -6,8 +6,8 @@
 
 struct virtual_bass_state {
 	sample_t level, envelope, time_const;
-	struct biquad_state lp[3];
-	struct biquad_state hp[3];
+	struct biquad_state lp[2];
+	struct biquad_state hp[2];
 };
 
 static inline sample_t harmonic_func(sample_t s, sample_t e, sample_t l)
@@ -15,22 +15,21 @@ static inline sample_t harmonic_func(sample_t s, sample_t e, sample_t l)
 	e = (e > 0.0) ? MINIMUM(1.0/e, 1000.0) : 1000.0;
 	sample_t e2 = e*e; sample_t e3 = e2*e;
 	sample_t s2 = s*s; sample_t s3 = s2*s; sample_t s4 = s3*s;
-	return e*s2*l + e2*s3*l*0.5 + e3*s4*l*0.25;
+	return e*s2*l + e2*s3*l*0.9 + e3*s4*l*0.5;
 }
 
 sample_t * virtual_bass_effect_run(struct effect *e, ssize_t *frames, sample_t *ibuf, sample_t *obuf)
 {
-	sample_t s, lf, hf;
+	sample_t s, lf;
 	ssize_t samples = *frames * e->ostream.channels, i, k;
 	struct virtual_bass_state **state = (struct virtual_bass_state **) e->data;
 	for (i = 0; i < samples; i += e->ostream.channels) {
 		for (k = 0; k < e->ostream.channels; ++k) {
 			if (state[k]) {
 				s = ibuf[i + k];
-				lf = biquad(&state[k]->lp[1], biquad(&state[k]->lp[0], s));
-				hf = biquad(&state[k]->hp[1], biquad(&state[k]->hp[0], s));
+				lf = biquad(&state[k]->hp[0], biquad(&state[k]->lp[1], biquad(&state[k]->lp[0], s)));
 				state[k]->envelope = MAXIMUM(state[k]->envelope, fabs(lf));
-				ibuf[i + k] = hf + lf + biquad(&state[k]->hp[2], biquad(&state[k]->lp[2], harmonic_func(lf, state[k]->envelope, state[k]->level)));
+				ibuf[i + k] = s + biquad(&state[k]->hp[1], harmonic_func(lf, state[k]->envelope, state[k]->level));
 				state[k]->envelope *= state[k]->time_const;
 			}
 		}
@@ -46,10 +45,8 @@ void virtual_bass_effect_reset(struct effect *e)
 		if (state[i]) {
 			biquad_reset(&state[i]->lp[0]);
 			biquad_reset(&state[i]->lp[1]);
-			biquad_reset(&state[i]->lp[2]);
 			biquad_reset(&state[i]->hp[0]);
 			biquad_reset(&state[i]->hp[1]);
-			biquad_reset(&state[i]->hp[2]);
 		}
 	}
 }
@@ -78,7 +75,7 @@ struct effect * virtual_bass_effect_init(struct effect_info *ei, struct stream_i
 
 	cutoff = parse_freq(argv[1], &endptr);
 	CHECK_ENDPTR(argv[1], endptr, "cutoff", return NULL);
-	CHECK_FREQ(cutoff, istream->fs/5.0, "cutoff", return NULL);
+	CHECK_FREQ(cutoff, istream->fs/4.0, "cutoff", return NULL);
 	if (argc == 3) {
 		level = strtod(argv[2], &endptr);
 		CHECK_ENDPTR(argv[2], endptr, "level", return NULL);
@@ -99,13 +96,11 @@ struct effect * virtual_bass_effect_init(struct effect_info *ei, struct stream_i
 		if (GET_BIT(channel_selector, i)) {
 			state[i] = calloc(1, sizeof(struct virtual_bass_state));
 			state[i]->level = level;
-			state[i]->time_const = pow(0.5, cutoff / 100.0 / istream->fs);
-			biquad_init_using_type(&state[i]->lp[0], BIQUAD_LOWPASS, istream->fs, cutoff, 0.5, 0.0, 0.0, BIQUAD_WIDTH_Q);
-			biquad_init_using_type(&state[i]->lp[1], BIQUAD_LOWPASS, istream->fs, cutoff, 1.0, 0.0, 0.0, BIQUAD_WIDTH_Q);
-			biquad_init_using_type(&state[i]->lp[2], BIQUAD_LOWPASS, istream->fs, cutoff * 2.5, 0.707106781187, 0.0, 0.0, BIQUAD_WIDTH_Q);
-			biquad_init_using_type(&state[i]->hp[0], BIQUAD_HIGHPASS, istream->fs, cutoff, 0.5, 0.0, 0.0, BIQUAD_WIDTH_Q);
-			biquad_init_using_type(&state[i]->hp[1], BIQUAD_HIGHPASS, istream->fs, cutoff, 1.0, 0.0, 0.0, BIQUAD_WIDTH_Q);
-			biquad_init_using_type(&state[i]->hp[2], BIQUAD_HIGHPASS, istream->fs, cutoff * 0.75, 0.707106781187, 0.0, 0.0, BIQUAD_WIDTH_Q);
+			state[i]->time_const = pow(0.5, MAXIMUM(cutoff / 100.0, 1.0) / istream->fs);
+			biquad_init_using_type(&state[i]->lp[0], BIQUAD_LOWPASS,    istream->fs, cutoff, 0.5,    0.0, 0.0, BIQUAD_WIDTH_Q);
+			biquad_init_using_type(&state[i]->lp[1], BIQUAD_LOWPASS,    istream->fs, cutoff, 1.0,    0.0, 0.0, BIQUAD_WIDTH_Q);
+			biquad_init_using_type(&state[i]->hp[0], BIQUAD_HIGHPASS_1, istream->fs, 20.0,   0.0,    0.0, 0.0, BIQUAD_WIDTH_Q);
+			biquad_init_using_type(&state[i]->hp[1], BIQUAD_HIGHPASS,   istream->fs, 20.0,   0.7071, 0.0, 0.0, BIQUAD_WIDTH_Q);
 		}
 	}
 	e->data = state;
